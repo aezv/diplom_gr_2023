0b992fec53bac8cba8dd588a4c79b8ffe5d7668c
aezv <aezv.global@gmail.com>
2023-06-12 12:10:55 +0300

    Правка скрипта сборки

--- build.js
+++ build.js
@@ -2,15 +2,28 @@ const child_process = require('child_process');
 
 let start_time = new Date();
 console.log("Запуск сборки");
+console.log("Сборка списка литературы");
 
-child_process.exec("xelatex diplom_gr_2023.tex", function (error) {
+child_process.exec("biber \"diplom_gr_2023\"", function (error) {
     if (error) {
-        console.log("Сборка закончилась с ошибкой: ");
+        console.log("Сборка списка литературы закончилась с ошибкой");
         console.log(error);
     }
     else {
-        console.log("Cборка завершилась без ошибок.");
-        console.log("Результат сборки - файл diplom.pdf");
+        console.log("Сборка списка литературы завершилась без ошибок");
+        console.log("Сборка диплома");
+        child_process.exec("xelatex -shell-escape diplom_gr_2023.tex", function (error) {
+            if (error) {
+                console.log("Сборка диплома закончилась с ошибкой: ");
+                console.log(error);
+                console.log("Сборка закончилась с ошибкой")
+            }
+            else {
+                console.log("Cборка диплома завершилась без ошибок");
+                console.log("Результат сборки - файл diplom_gr_2023.pdf");
+                console.log("Сборка завершилась без ошибок")
+            }
+            console.log("Время: " + ((new Date() - start_time) / 1000) + "с");
+        });
     }
-    console.log("Время: " + ((new Date() - start_time) / 1000) + "с");
 });
